6faabb18945e0c91435fbfedf231cbdd811c8914
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2021-02-22 23:37:15 +0700

    menyelesaikan pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,64 @@
+<?php 
+/*
+Rudi Haryanto
+203040147
+https://github.com/Rudi-Haryanto/pw2021_203040147
+Pertemuan 2 - 22 Februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+<?php 
+// pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh di awali dengan angka, tapi boleh mengandung angka
+// $nama = "Rudi Haryanto";
+// echo "Hallo, nama saya $nama";
+
+// operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan = "Rudi";
+// $nama_belakang = "Haryanto";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assigment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x += 5;
+// echo $x;
+// $nama = "Rudi";
+// $nama .= " ";
+// $nama .= "Haryanto";
+// echo $nama;
+
+// perbandingan
+// <, >, <=, >=, ==
+// var_dump(1 > 5);
+
+// identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0); 
+?>
