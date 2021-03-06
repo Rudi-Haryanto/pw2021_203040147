f29f2708fdfac25e0a7c2dc49557f5f64c5a6d1d
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2021-03-06 16:13:20 +0700

    menyelesaikan pertemuan4 tentang function

--- pertemuan4/date.php
+++ pertemuan4/date.php
@@ -6,7 +6,19 @@
 // Time
 // UNIX Timestamp / EPOCH time
 // detik yang sudah berlalu sejak 1 januari 1970
-echo time();
+// echo time();
+// echo date("d M Y", time()-60*60*24*100);
+
+// mktime
+// membuat sendiri detik
+// mktime(0,0,0,0,0,0)
+// jam, menit, detik, bulan, tanggal, tahun
+// echo date("l", mktime(0,0,0,4,9,2002));
+
+
+// strtotime
+echo date("l", strtotime("9 april 2002"));
+
 
 
  ?>
