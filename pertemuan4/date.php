2890ca67c66d77c3d3afc6944d57c43982d45666
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2021-03-06 14:59:31 +0700

    sedang mengerjakan pertemuan4 di bagian time

---
+++ pertemuan4/date.php
@@ -0,0 +1,12 @@
+<?php 
+// date
+// untuk menampilkan tanggal dengan format tertentu
+// echo date( "l, d-M-Y" );
+
+// Time
+// UNIX Timestamp / EPOCH time
+// detik yang sudah berlalu sejak 1 januari 1970
+echo time();
+
+
+ ?>
