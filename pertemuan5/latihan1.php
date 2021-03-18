e01a053388027b72fd48aadb41bb42cc436b2fde
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2021-03-18 22:24:13 +0700

    proses untuk menyelesaikan tugas pertemuan 5

---
+++ pertemuan5/latihan1.php
@@ -0,0 +1,22 @@
+<?php 
+// array
+// variabel yang dapat memiliki banyak nilai
+// elemen pada array boleh memiliki tipe data yang berbeda
+
+// membuat array
+// cara lama
+$hari = array("Senin", "Selasa", "Rabu");
+// cara baru
+$bulan = ["Januari", "Februari", "Maret"];
+$arr1 = [123, "tulisan", false];
+
+// menampilkan Array
+echo $hari;
+
+
+
+
+
+
+
+ ?>
