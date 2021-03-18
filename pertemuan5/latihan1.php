35f3ab934ab87ca9a0214d15f95cda107cd9a502
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2021-03-18 22:40:32 +0700

    menyelesaikan latihan1.php

--- pertemuan5/latihan1.php
+++ pertemuan5/latihan1.php
@@ -2,6 +2,8 @@
 // array
 // variabel yang dapat memiliki banyak nilai
 // elemen pada array boleh memiliki tipe data yang berbeda
+// pasangan antara key dan value
+// key-nya adalah index, yang dimulai dari 0
 
 // membuat array
 // cara lama
@@ -11,9 +13,23 @@ $bulan = ["Januari", "Februari", "Maret"];
 $arr1 = [123, "tulisan", false];
 
 // menampilkan Array
-echo $hari;
+// var_dump() / print_r()
+// var_dump($hari);
+// echo "<br>";
+// print_r($bulan);
+// echo "<br>";
 
+// menampilkan 1 elemen pada array
+// echo $arr1[0];
+// echo "<br>";
+// echo $bulan[1]
 
+// menambahkan elemen baru pada array
+var_dump($hari);
+$hari[] = "Kamis";
+$hari[] = "Jum'at";
+echo "<br>";
+var_dump($hari);
 
 
 
