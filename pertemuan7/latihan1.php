f2797d9c2324bae2f78a917fd3581e7ccd076d81
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2021-03-19 19:33:26 +0700

    mengerjakan tugas pertemuan7

---
+++ pertemuan7/latihan1.php
@@ -0,0 +1,37 @@
+<?php 
+// $_GET
+$mahasiswa = [
+	[
+		"nama" => "Rudi Haryanto", 
+		"nrp" => "203040147",
+		"email" => "rudiharyantovrt99@gmail.com",
+		"jurusan" => "Teknik Informatika",
+		"gambar" => "image.png"
+	],
+	[
+		"nama" => "Mono", 
+		"nrp" => "203040146",
+		"email" => "monovrt99@gmail.com",
+		"jurusan" => "Teknik Industri",
+		"gambar" => "mono.png"
+	]
+];
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>GET</title>
+ </head>
+ <body>
+ <h1>Daftar Mahasiswa</h1>
+<?php foreach( $mahasiswa as $mhs ) : ?>
+	<ul>
+		<li></li>
+	</ul>
+
+
+
+
+ 
+ </body>
+ </html>
