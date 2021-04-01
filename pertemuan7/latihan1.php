e13207997801d5cf028e5b98362a61f1715f6351
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2021-04-01 18:54:59 +0700

    menyelesaikan pertemuan7

--- pertemuan7/latihan1.php
+++ pertemuan7/latihan1.php
@@ -24,13 +24,15 @@ $mahasiswa = [
  </head>
  <body>
  <h1>Daftar Mahasiswa</h1>
+ <ul>
 <?php foreach( $mahasiswa as $mhs ) : ?>
-	<ul>
-		<li></li>
-	</ul>
+	<li>
+		<a href="latihan2.php?nama=<?= $mhs["nama"]; ?>&nrp=<?= $mhs["nrp"]; ?>&email=<?= $mhs["email"]; ?>&jurusan=<?= $mhs["jurusan"]; ?>&gambar=<?= $mhs["gambar"]; ?>"><?= $mhs["nama"]; ?></a>
+			
+	</li>
 
-
-
+<?php endforeach; ?>
+</ul>
 
  
  </body>
