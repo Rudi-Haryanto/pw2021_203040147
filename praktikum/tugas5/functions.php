180c1217337fda8da384af44b00ac7d542990ab3
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2021-04-17 17:02:53 +0700

    telah selesai tugas modul 5

--- praktikum/tugas5/functions.php
+++ praktikum/tugas5/functions.php
@@ -64,6 +64,16 @@ function ubah($data) {
 }
 
 
+function cari($keyword) {
+	$query = "SELECT * FROM mahasiswa
+				WHERE
+			  nama LIKE '%$keyword%' OR 
+			  nrp LIKE '%$keyword%' OR
+			  email LIKE '%$keyword%' OR
+			  jurusan LIKE '%$keyword%'
+			";
+	return query($query);
+}
 
 
 
--- praktikum/tugas5/index.php
+++ praktikum/tugas5/index.php
@@ -1,6 +1,12 @@
 <?php 
 require 'functions.php';
 $mahasiswa = query("SELECT * FROM mahasiswa");
+
+// tombol cari ditekan
+if( isset($_POST["cari"]) ) {
+	$mahasiswa = cari($_POST["keyword"]);
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,6 +20,13 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
 <a href="tambah.php">Tambah data mahasiswa</a>
 <br></br>
 
+<form action="" method="post">
+
+	<input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian.." autocomplete="off">
+	<button type="submit" name="cari">Cari!</button>
+	
+</form>
+<br>
 <table border="1" cellpadding="10" cellspacing="0">
 	
 	<tr>
